b483600f46f86ce43611c0e167e3d77bd1785daa
hrswcksono <57925757+hrswcksono@users.noreply.github.com>
2020-02-29 01:55:37 +0700

    up up

--- soal2/soal2.sh
+++ soal2/soal2.sh
@@ -8,7 +8,7 @@ if [[ $input =~ ^[A-Za-z_]+$ ]]; then
   flag=1;
   while [ $flag -gt 0 ]
   do
-     a=$(cat /dev/urandom| tr -dc '0-9a-zA-Z'|head -c 3)
+     a=$(cat /dev/urandom| tr -dc '0-9a-zA-Z'|head -c 28)
      if [[ "$a" =~ [[:upper:]] && "$a" =~ [[:lower:]] && "$a" =~ [[:digit:]] ]]; then
       echo "$a" >> "$input".txt
       flag=0
